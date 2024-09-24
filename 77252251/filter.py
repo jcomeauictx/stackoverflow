83783dee67dc7986aea60b8447c1c332f58c5de0
John Comeau <jc@unternet.net>
2024-09-24 06:53:22 -0700

    first two ways work, 3rd hangs

--- 77252251/filter.py
+++ 77252251/filter.py
@@ -13,7 +13,7 @@ from http import HTTPStatus
 from urllib.request import urlopen
 try:
     from mitmproxy import http, ctx
-    from mitmproxy.script import concurrent
+#   from mitmproxy.script import concurrent
 except (ImportError, ModuleNotFoundError):  # for doctests
     logging.warning('simulating mitmproxy for doctests')
     # pylint: disable=invalid-name
@@ -23,12 +23,8 @@ except (ImportError, ModuleNotFoundError):  # for doctests
         'simulated decorator'
         return None
 
-STRATEGIES = ['redirect', 'copyflow', 'request']
-STATE = {
-    # global for saved state
-    'index': 0,  # increment to try different strategies
-    'strategy': STRATEGIES[0],
-}
+STRATEGIES = ['request', 'redirect', 'copyflow']
+INDEX = 0
 
 # pylint: disable=consider-using-f-string
 def request(flow: http.HTTPFlow):
@@ -36,49 +32,53 @@ def request(flow: http.HTTPFlow):
     filter requests
     '''
     logging.warning('request for path: %s, replay: %s, strategy: %s',
-                  flow.request.path, flow.is_replay, STATE['strategy'])
+                  flow.request.path, flow.is_replay, STRATEGIES[INDEX])
     logging.warning('flow: %s', oneline(flow))
 
-@concurrent  # needed only for copyflow, but hopefully won't break others
+#@concurrent  # needed only for copyflow, but hopefully won't break others
 def response(flow: http.HTTPFlow):
     '''
     filter responses
     '''
     logging.warning('response for path: %s, replay: %s, strategy: %s',
-                  flow.request.path, flow.is_replay, STATE['strategy'])
+                  flow.request.path, flow.is_replay, STRATEGIES[INDEX])
     logging.warning('flow: %s', oneline(flow))
+    strategy = STRATEGIES[INDEX]
     if len(flow.response.content.rstrip()) > 1:
         logging.warning('returning response %s to client', oneline(flow))
         # remove timestamp
         flow.request.path = timestamp(flow.request.path, remove=True)
         next_strategy()
-    elif STATE['strategy'] == 'redirect':
-        redirect = timestamp(flow.request.path)
-        flow.response = http.Response.make(
-            HTTPStatus.FOUND,
-            '<a href="%s">%s</a>' % (redirect, redirect),
-            {'content-type': 'text/html', 'location': redirect}
-        )
-    elif STATE['strategy'] == 'request':
-        # use Python standard library urllib.request.urlopen
-        answer = flow.response.content
-        while len(answer.rstrip()) < 2:
-            flow.request.path = timestamp(flow.request.path)
-            with urlopen(flow.request.url) as instream:
-                answer = instream.read()
-        flow.response.content = answer
-        next_strategy()
-    else:
-        copy = flow.copy()
-        copy.response = None
-        if 'view' in ctx.master.addons:
-            logging.warning('duplicating flow in view')
-            ctx.master.commands.call('views.flows.duplicate', [copy])
-        copy.request.path = timestamp(copy.request.path)
-        ctx.master.commands.call('replay.client', [copy])
-        # killing flow at this point will cause client to shut down.
-        # we could instead try `.intercept()`
-        flow.intercept()
+        return
+    match strategy:
+        case 'redirect':
+            redirect = timestamp(flow.request.path)
+            flow.response = http.Response.make(
+                HTTPStatus.FOUND,
+                '<a href="%s">%s</a>' % (redirect, redirect),
+                {'content-type': 'text/html', 'location': redirect}
+            )
+        case 'request':
+            # use Python standard library urllib.request.urlopen
+            answer = flow.response.content
+            while len(answer.rstrip()) < 2:
+                flow.request.path = timestamp(flow.request.path)
+                with urlopen(flow.request.url) as instream:
+                    answer = instream.read()
+            flow.response.content = answer
+            next_strategy()
+        case 'copyflow':
+            copy = flow.copy()
+            copy.response = None
+            if 'view' in ctx.master.addons:
+                logging.warning('duplicating flow in view')
+                ctx.master.commands.call('views.flows.duplicate', [copy])
+            copy.request.path = timestamp(copy.request.path)
+            ctx.master.commands.call('replay.client', [copy])
+            # killing flow at this point will cause client to shut down.
+            # we could instead try `.intercept()`
+            flow.intercept()
+    return
 
 def timestamp(path, remove=False):
     '''
@@ -100,5 +100,5 @@ def next_strategy():
     '''
     try another strategy to see if it works
     '''
-    STATE['strategy'] = STRATEGIES[STATE['index']]
-    STATE['index'] = (STATE['index'] + 1) % len(STRATEGIES)
+    global INDEX  # pylint: disable=global-statement
+    INDEX = (INDEX + 1) % len(STRATEGIES)
