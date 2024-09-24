3b086846f7c117526600979aa897ef29e067d894
John Comeau <jc@unternet.net>
2024-09-24 10:22:39 -0700

    returns '5' on first copyflow request, then client connection is closed

--- 77252251/filter.py
+++ 77252251/filter.py
@@ -22,67 +22,67 @@ STRATEGIES = ['request', 'redirect', 'copyflow']
 INDEX = 0
 
 # pylint: disable=consider-using-f-string
-def request(flow: http.HTTPFlow):
-    '''
-    filter requests
-    '''
-    logging.warning('request for path: %s, replay: %s, strategy: %s',
-                  flow.request.path, flow.is_replay, STRATEGIES[INDEX])
-    logging.warning('flow: %s', oneline(flow))
-
 # concurrent needed only for copyflow, but hopefully won't break others
 try:
     @concurrent
-    def response(flow: http.HTTPFlow):
+    def request(flow: http.HTTPFlow):
         '''
-        filter responses
+        filter requests
         '''
-        logging.warning('response for path: %s, replay: %s, strategy: %s',
+        logging.warning('request for path: %s, replay: %s, strategy: %s',
                       flow.request.path, flow.is_replay, STRATEGIES[INDEX])
         logging.warning('flow: %s', oneline(flow))
-        strategy = STRATEGIES[INDEX]
-        if len(flow.response.content.rstrip()) > 1:
-            logging.warning('returning response %s to client', oneline(flow))
-            # remove timestamp
-            flow.request.path = timestamp(flow.request.path, remove=True)
-            next_strategy()
-            return
-        match strategy:
-            case 'redirect':
-                redirect = timestamp(flow.request.path)
-                flow.response = http.Response.make(
-                    HTTPStatus.FOUND,
-                    '<a href="%s">%s</a>' % (redirect, redirect),
-                    {'content-type': 'text/html', 'location': redirect}
-                )
-            case 'request':
-                # use Python standard library urllib.request.urlopen
-                answer = flow.response.content
-                while len(answer.rstrip()) < 2:
-                    flow.request.path = timestamp(flow.request.path)
-                    logging.warning('making followup request to %s',
-                                    flow.request.path)
-                    with urlopen(flow.request.url) as instream:
-                        answer = instream.read()
-                flow.response.content = answer
-                next_strategy()
-            case 'copyflow':
-                copy = flow.copy()
-                copy.response = None
-                if 'view' in ctx.master.addons:
-                    logging.warning('duplicating flow in view')
-                    ctx.master.commands.call('views.flows.duplicate', [copy])
-                copy.request.path = timestamp(copy.request.path)
-                ctx.master.commands.call('replay.client', [copy])
-                # killing flow at this point will cause client to shut down.
-                # we could instead try `.intercept()`
-                #flow.intercept()
-        return
+        if STRATEGIES[INDEX] == 'copyflow':
+            # queue another request in case this one's response is unsuitable
+            copy = flow.copy()
+            if 'view' in ctx.master.addons:
+                logging.warning('duplicating flow in view')
+                ctx.master.commands.call('views.flows.duplicate', [copy])
+            copy.request.path = timestamp(copy.request.path)
+            ctx.master.commands.call('replay.client', [copy])
 except NotImplementedError:
     if COMMAND == 'doctest':
         logging.error('concurrent decorator not supported during doctests')
     else:
         raise
+
+def response(flow: http.HTTPFlow):
+    '''
+    filter responses
+    '''
+    logging.warning('response for path: %s, replay: %s, strategy: %s',
+                  flow.request.path, flow.is_replay, STRATEGIES[INDEX])
+    logging.warning('flow: %s', oneline(flow))
+    strategy = STRATEGIES[INDEX]
+    if len(flow.response.content.rstrip()) > 1:
+        logging.warning('returning response %s to client', oneline(flow))
+        # remove timestamp
+        flow.request.path = timestamp(flow.request.path, remove=True)
+        next_strategy()
+        return
+    match strategy:
+        case 'redirect':
+            redirect = timestamp(flow.request.path)
+            flow.response = http.Response.make(
+                HTTPStatus.FOUND,
+                '<a href="%s">%s</a>' % (redirect, redirect),
+                {'content-type': 'text/html', 'location': redirect}
+            )
+        case 'request':
+            # use Python standard library urllib.request.urlopen
+            answer = flow.response.content
+            while len(answer.rstrip()) < 2:
+                flow.request.path = timestamp(flow.request.path)
+                logging.warning('making followup request to %s',
+                                flow.request.path)
+                with urlopen(flow.request.url) as instream:
+                    answer = instream.read()
+            flow.response.content = answer
+            next_strategy()
+        case 'copyflow':
+            logging.warning('ignoring response %r during copyflow strategy',
+                            flow.response.content)
+    return
 
 def timestamp(path, remove=False):
     '''
