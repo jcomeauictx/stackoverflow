f9a8711f6274dd1a13c7c6b12e40bc6eb6137eab
John Comeau <jc@unternet.net>
2024-09-24 09:31:15 -0700

    got rid of error during doctest run, still hangs at copyflow

--- 77252251/filter.py
+++ 77252251/filter.py
@@ -8,21 +8,16 @@ cannot be "both".
 try using urllib.request.urlopen: it returns an http.client.HTTPResponse
 object which can then be .read().
 '''
-import time, logging  # pylint: disable=multiple-imports
+import sys, os, time, logging  # pylint: disable=multiple-imports
 from http import HTTPStatus
 from urllib.request import urlopen
-try:
-    from mitmproxy import http, ctx
-#   from mitmproxy.script import concurrent
-except (ImportError, ModuleNotFoundError):  # for doctests
-    logging.warning('simulating mitmproxy for doctests')
-    # pylint: disable=invalid-name
-    http = type('', (), {'HTTPFlow': None})
-    ctx = type('', (), {})
-    def concurrent(function):  # pylint: disable=unused-argument
-        'simulated decorator'
-        return None
+from mitmproxy import http, ctx
+from mitmproxy.script import concurrent
 
+# NOTE: using anything lower than WARNING level in logging will not work
+# with mitmproxy, unless further configuration is done
+COMMAND = os.path.splitext(os.path.basename(sys.argv[0]))[0]
+logging.warning('COMMAND: %s', COMMAND)
 STRATEGIES = ['request', 'redirect', 'copyflow']
 INDEX = 0
 
@@ -35,50 +30,57 @@ def request(flow: http.HTTPFlow):
                   flow.request.path, flow.is_replay, STRATEGIES[INDEX])
     logging.warning('flow: %s', oneline(flow))
 
-#@concurrent  # needed only for copyflow, but hopefully won't break others
-def response(flow: http.HTTPFlow):
-    '''
-    filter responses
-    '''
-    logging.warning('response for path: %s, replay: %s, strategy: %s',
-                  flow.request.path, flow.is_replay, STRATEGIES[INDEX])
-    logging.warning('flow: %s', oneline(flow))
-    strategy = STRATEGIES[INDEX]
-    if len(flow.response.content.rstrip()) > 1:
-        logging.warning('returning response %s to client', oneline(flow))
-        # remove timestamp
-        flow.request.path = timestamp(flow.request.path, remove=True)
-        next_strategy()
-        return
-    match strategy:
-        case 'redirect':
-            redirect = timestamp(flow.request.path)
-            flow.response = http.Response.make(
-                HTTPStatus.FOUND,
-                '<a href="%s">%s</a>' % (redirect, redirect),
-                {'content-type': 'text/html', 'location': redirect}
-            )
-        case 'request':
-            # use Python standard library urllib.request.urlopen
-            answer = flow.response.content
-            while len(answer.rstrip()) < 2:
-                flow.request.path = timestamp(flow.request.path)
-                with urlopen(flow.request.url) as instream:
-                    answer = instream.read()
-            flow.response.content = answer
+# concurrent needed only for copyflow, but hopefully won't break others
+try:
+    @concurrent
+    def response(flow: http.HTTPFlow):
+        '''
+        filter responses
+        '''
+        logging.warning('response for path: %s, replay: %s, strategy: %s',
+                      flow.request.path, flow.is_replay, STRATEGIES[INDEX])
+        logging.warning('flow: %s', oneline(flow))
+        strategy = STRATEGIES[INDEX]
+        if len(flow.response.content.rstrip()) > 1:
+            logging.warning('returning response %s to client', oneline(flow))
+            # remove timestamp
+            flow.request.path = timestamp(flow.request.path, remove=True)
             next_strategy()
-        case 'copyflow':
-            copy = flow.copy()
-            copy.response = None
-            if 'view' in ctx.master.addons:
-                logging.warning('duplicating flow in view')
-                ctx.master.commands.call('views.flows.duplicate', [copy])
-            copy.request.path = timestamp(copy.request.path)
-            ctx.master.commands.call('replay.client', [copy])
-            # killing flow at this point will cause client to shut down.
-            # we could instead try `.intercept()`
-            flow.intercept()
-    return
+            return
+        match strategy:
+            case 'redirect':
+                redirect = timestamp(flow.request.path)
+                flow.response = http.Response.make(
+                    HTTPStatus.FOUND,
+                    '<a href="%s">%s</a>' % (redirect, redirect),
+                    {'content-type': 'text/html', 'location': redirect}
+                )
+            case 'request':
+                # use Python standard library urllib.request.urlopen
+                answer = flow.response.content
+                while len(answer.rstrip()) < 2:
+                    flow.request.path = timestamp(flow.request.path)
+                    with urlopen(flow.request.url) as instream:
+                        answer = instream.read()
+                flow.response.content = answer
+                next_strategy()
+            case 'copyflow':
+                copy = flow.copy()
+                copy.response = None
+                if 'view' in ctx.master.addons:
+                    logging.warning('duplicating flow in view')
+                    ctx.master.commands.call('views.flows.duplicate', [copy])
+                copy.request.path = timestamp(copy.request.path)
+                ctx.master.commands.call('replay.client', [copy])
+                # killing flow at this point will cause client to shut down.
+                # we could instead try `.intercept()`
+                flow.intercept()
+        return
+except NotImplementedError:
+    if COMMAND == 'doctest':
+        logging.error('concurrent decorator not supported during doctests')
+    else:
+        raise
 
 def timestamp(path, remove=False):
     '''
